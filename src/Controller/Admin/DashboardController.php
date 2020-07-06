121f708bbd88936137491a98d4c178bbdd4e41b3
marjolaineltr <marjolainehaziza@gmail.com>
2020-07-06 22:54:13 +0200

    Add users list on route admin_user + display users as MenuItem

---
+++ src/Controller/Admin/DashboardController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Controller\UserController;
+use App\Entity\User;
+use App\Repository\UserRepository;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Router\CrudUrlGenerator;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DashboardController extends AbstractDashboardController
+{
+    /**
+     * @Route("/admin", name="admin", methods={"GET"})
+     */
+    public function index(): Response
+    {
+        //$users = $userRepository->findAll();
+        return parent::index();
+
+        // redirect to some CRUD controller
+        //$routeBuilder = $this->get(CrudUrlGenerator::class)->build();
+
+        //return $this->redirect($routeBuilder->setController(UserController::class)->generateUrl());
+
+        // you can also redirect to different pages depending on the current user
+        //if ('jane' === $this->getUser()->getUsername()) {
+        //    return $this->redirect('...');
+        //}
+
+        // you can also render some template to display a proper Dashboard
+        // (tip: it's easier if your template extends from @EasyAdmin/page/content.html.twig)
+        //return $this->render('user/test.html.twig', [
+        //    'users' => $users,
+        //]);
+    }
+
+    public function configureDashboard(): Dashboard
+    {
+        return Dashboard::new()
+            ->setTitle('O Wine');
+    }
+
+    public function configureMenuItems(): iterable
+    {
+        return [
+            MenuItem::linktoDashboard('Dashboard', 'fa fa-home'),
+    
+            MenuItem::linktoCrud('Users', 'fa fa-user', User::class)
+            ->setController(UserController::class),
+
+            // MenuItem::linkToLogout('Logout', 'fa fa-exit'),
+        ];
+
+    }
+}
---
+++ src/Controller/UserController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\UserRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class UserController extends AbstractController
+{
+    /**
+     * @Route("/admin/user", name="admin_user", methods="GET")
+     */
+    public function admin(UserRepository $userRepository)
+    {
+        return $this->render('user/admin.html.twig', [
+            'users' => $userRepository->findAll()
+        ]);
+    }
+
+    
+}
