4c91af6244c9c2b1af64e785c705d4fa5831636f
marjolaineltr <marjolainehaziza@gmail.com>
2020-07-07 15:41:01 +0200

    Add Registration Controller + Registration Form Type + EmailVerifier + Register template + confirmation email template + div container on base twig + easyadmin access restriction to ROLE_ADMIN

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -6,10 +6,12 @@ use App\Repository\UserRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Entity(repositoryClass=UserRepository::class)
+ * @UniqueEntity(fields={"email"}, message="There is already an account with this email")
  */
 class User implements UserInterface
 {
@@ -115,6 +117,11 @@ class User implements UserInterface
      * @ORM\ManyToMany(targetEntity=Order::class, mappedBy="seller")
      */
     private $receivedOrders;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $isVerified = false;
 
     public function __construct()
     {
@@ -463,6 +470,18 @@ class User implements UserInterface
             $this->receivedOrders->removeElement($receivedOrder);
             $receivedOrder->removeSeller($this);
         }
+
+        return $this;
+    }
+
+    public function isVerified(): bool
+    {
+        return $this->isVerified;
+    }
+
+    public function setIsVerified(bool $isVerified): self
+    {
+        $this->isVerified = $isVerified;
 
         return $this;
     }
