650c0663fc2556e5d9de96a7fbea41e1ebdf1883
LouGrom <lou.gromenil@gmail.com>
2020-07-13 16:05:11 +0200

    Update Product form

--- src/Form/ProductType.php
+++ src/Form/ProductType.php
@@ -2,6 +2,7 @@
 
 namespace App\Form;
 
+use App\Entity\Appellation;
 use App\Entity\Color;
 use App\Entity\User;
 use App\Entity\Product;
@@ -33,7 +34,11 @@ class ProductType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('appellation', TextType::class, ["label"=>"Nom du produit"])
+            ->add('appellation', EntityType::class, [
+                "label"=>"Nom du produit",
+                "class"=>Appellation::class,
+                "choice_label"=>'name'
+            ])
             ->add('area', TextType::class, ["label"=>"Zone"])
             ->add('type', EntityType::class, [
                 'label'=> 'Type de vin',
